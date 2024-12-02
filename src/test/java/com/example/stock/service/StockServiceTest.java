6995dc7148c1b2aaa6ae21badb69a29325351d59
KimGwangHoon <khkimykk@naver.com>
2024-12-03 00:43:32 +0900

    [v1] 재고감소 구현과 동시성 테스트 추가

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,70 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.hibernate.annotations.Source;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void setUp() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("재고 감소")
+    public void 재고감소() {
+        stockService.decreaseStock(1L, 1L);
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(99L, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시_100개의_재고_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decreaseStock(1L, 1L);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0L, stock.getQuantity());
+    }
+
+
+}
